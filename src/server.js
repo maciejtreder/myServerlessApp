877b034a2d3a741cce59b6ed52b5ba189824ce66
maciejtreder <contact@maciejtreder.com>
2017-07-02 20:55:10 +0200

    Build friends list app

--- src/server.js
+++ src/server.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const app = express();
+class InMemoryFriends {
+    constructor() {
+        this.list = [];
+    }
+
+    add(name) {
+        this.list.push(name);
+    }
+
+    getAll() {
+        return this.list;
+    }
+}
+const friendsList = new InMemoryFriends();
 
 let port = process.env.PORT || 8000;
 const baseUrl = `http://localhost:${port}`
 
+const bodyParser = require('body-parser');
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', 'src');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.status(200).send("hello world!");
+    res.render('homepage', {personList: friendsList.getAll()});
 });
+
+app.post('/submit', (req, res) => {
+    friendsList.add(req.body.friendName);
+    res.render('person-added', { personName: req.body.friendName, personList: friendsList.getAll() });
+});
+
 
 // Server
 app.listen(port, () => {
